d841aa16fdeb2f64416ce845f69282c71b36e963
陈龙 <chen_teacher@163.com>
2024-01-04 00:08:39 +0800

    chore: 引入micro-app微前端框架

--- src/layouts/main.vue
+++ src/layouts/main.vue
@@ -3,16 +3,46 @@
 </script>
 
 <template>
-  <div>
-    <header>这是头部</header>
-    <aside>这是侧边栏</aside>
-    <main>
-      <router-view />
+  <div class="main-layout">
+    <MainLayoutHeader class="header">这是头部</MainLayoutHeader>
+    <MainLayoutAside class="aside">这是侧边栏</MainLayoutAside>
+    <main class="main">
+      <div class="main-content">
+        <router-view />
+      </div>
     </main>
-    <footer>这是页脚</footer>
+    <MainLayoutFooter class="footer">这是页脚</MainLayoutFooter>
   </div>
 </template>
 
-<style scoped>
-
+<style>
+.main-layout{
+  --aside-width: 300px;
+  --header-height: 60px;
+  --footer-height: 40px;
+  display: grid;
+  grid-template-areas: "header header" "aside main" "aside footer";
+  grid-template-rows: var(--header-height) 1fr var(--footer-height);
+  grid-template-columns: var(--aside-width) 1fr;
+  height: 100dvh;
+  .header{
+    grid-area: header;
+  }
+  .aside{
+    grid-area: aside;
+  }
+  .main{
+    grid-area: main;
+    overflow: hidden;
+    position: relative;
+    .main-content{
+      overflow: auto;
+      height: 100%;
+      width: 100%;
+    }
+  }
+  .footer {
+    grid-area: footer;
+  }
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import App from './App.vue'
 import 'virtual:uno.css'
 import Store from '@/store/index.ts'
 import Router from '@/router'
+import microApp from '@micro-zoe/micro-app'
 
+microApp.start()
 createApp(App)
   .use(Store)
   .use(Router)
